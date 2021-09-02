7500c66d10fa8d10b99e05d234e88d95b1e6b2b1
batch7d <abc@gmail.com>
2021-09-02 22:14:51 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+package calci;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double number1,number2,result=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the numbers");
+		number1=sc.nextDouble();
+		number2=sc.nextDouble();
+		char operator=sc.next().charAt(0);
+		switch(operator)
+		{
+		case '+':
+			result=number1+number2;
+			break;
+		case '-':
+			result=number1-number2;
+			break;
+		default:
+			System.out.println("You enter wrong input");
+			break;
+		}
+		System.out.println("The final result:");
+		  
+        System.out.println();
+        System.out.println(number1 + " " + operator + " " + number2
+                + " = " + result);
+
+	}
+
+}
